25b72deca8629597e6c56433e09fb31bbe9af5eb
andee <andee@e1.ru>
2018-02-28 05:55:54 +0000

    testing sizing

--- main.cpp
+++ main.cpp
@@ -29,6 +29,8 @@ using boost::algorithm::any_of_equal;
 typedef CCEX EXCHANGE;
 
 int main(int argc, char *argv[]) {
+    typedef std::unordered_map<std::string, std::pair<double, double>> cycleSize_t;
+
     Scheduler sched;
     Barrier proxies_loaded;
     //thread_pool tp(std::thread::hardware_concurrency());
@@ -57,69 +59,96 @@ int main(int argc, char *argv[]) {
         sched.terminate();
     });
     Thread main_thread(sched, "main thread", [&] {
-        api.load_keys();
-        api.load_nonces();
-        proxies_loaded.wait();
-        std::unordered_map<std::string, double> balance;
+        elle::With<Scope>() << [&](Scope & scope) {
+            api.load_keys();
+            api.load_nonces();
+            proxies_loaded.wait();
+            std::unordered_map<std::string, double> balance;
 
-        //std::cout<<"pairs="<<api.get_all_pairs()<<'\n';
-        //std::cout<<"opened orders: "<<api.get_open_orders()<<'\n';
-        while(true) {
-            api.get_full_ob(arb);
-            api.update_balance(balance);
-            /*std::cout<<"mcap-btc rate = "<<arb("mcap-btc").rate<<" "<<arb.ob("mcap-btc", 0)<<" "<<arb.ob("mcap-btc", 0, true)<<'\n';
-            api.get_ob({"mcap-btc"}, arb);
-            std::cout<<"mcap-btc rate = "<<arb("mcap-btc").rate<<" "<<arb.ob("mcap-btc", 0)<<" "<<arb.ob("mcap-btc", 0, true)<<'\n';
-            arb.change_rate("mcap-btc", gain_r(arb.ob_size("mcap-btc",0, true)));
-            std::cout<<"mcap-btc rate = "<<arb("mcap-btc").rate<<'\n';
-            arb.change_rate("mcap-btc", gain_r(arb.ob_size("mcap-btc",0, false)));
-            std::cout<<"mcap-btc rate = "<<arb("mcap-btc").rate<<'\n';*/
-            auto cycles = arb.find_cycles();
-            std::cout<<"Found "<<cycles.size()<<" cycles\n";
-            api.get_ob(arb.extract_unique_pairs(cycles), arb);
-            for(auto cycle: cycles) {
-                std::cout<<">>> CYCLE: "<<cycle2string(cycle)<<" gain="<<gain(arb, cycle)<<'\n';
-                //for(auto p_it = cycle.begin(); p_it != std::prev(cycle.end()); ++p_it) {
-                for(size_t i = 0; i < cycle.size(); ++i) {
-                    auto p = *cycle.begin();
-                    arb.change_rate(p, gain_r(arb.ob_size(p, 0, true)));
-                    for(auto nxt_p = std::next(cycle.begin()); nxt_p != cycle.end(); ++nxt_p) {
-                        for(size_t j = 0; j < arb(p).ob.size(); ++j) {
-                            auto sizes = arb.sizing_fixed(cycle, *nxt_p, arb.ob_size(*nxt_p, j), {{p, gain_r(arb.ob_size(p, 0, true))}});
-                            arb.minsize(cycle, sizes);
-                            arb.align_sizes(cycle, sizes);
-                            double profit = calc_profit(cycle, sizes);
-                            if(profit>0 && arb.is_aligned(cycle, sizes)) {
-                                std::cout<<"locked "<<p<<", @rate="<<arb(p).rate<<", maximizing "<<*nxt_p<<", row="<<j<<" ->"<<arb.ob_size(*nxt_p, j)<<" gain="<<gain(arb, cycle)<<" real_gain="<<calc_gain(cycle, sizes)<<"\n";
-                                std::cout<<print_sizes(arb, cycle, sizes)<<" profit="<<profit<<'\n';
-                                if(balance.count(as_pair(cycle[0]).first))
-                                    if(balance[as_pair(cycle[0]).first] >= sizes[cycle[0]].first) {
-                                        std::cout<<"groups: "<<groupping(cycle, sizes, balance, true)<<'\n';
-                                    }
+            //std::cout<<"pairs="<<api.get_all_pairs()<<'\n';
+            //std::cout<<"opened orders: "<<api.get_open_orders()<<'\n';
+            while(true) {
+                std::unordered_map<std::string, cycleSize_t> cs;
+                scope.run_background("update_balance", [&]{ api.update_balance(balance); });
+                api.get_full_ob(arb);
+                /*std::cout<<"mcap-btc rate = "<<arb("mcap-btc").rate<<" "<<arb.ob("mcap-btc", 0)<<" "<<arb.ob("mcap-btc", 0, true)<<'\n';
+                api.get_ob({"mcap-btc"}, arb);
+                std::cout<<"mcap-btc rate = "<<arb("mcap-btc").rate<<" "<<arb.ob("mcap-btc", 0)<<" "<<arb.ob("mcap-btc", 0, true)<<'\n';
+                arb.change_rate("mcap-btc", gain_r(arb.ob_size("mcap-btc",0, true)));
+                std::cout<<"mcap-btc rate = "<<arb("mcap-btc").rate<<'\n';
+                arb.change_rate("mcap-btc", gain_r(arb.ob_size("mcap-btc",0, false)));
+                std::cout<<"mcap-btc rate = "<<arb("mcap-btc").rate<<'\n';*/
+                auto cycles = arb.find_cycles();
+                std::cout << "Found " << cycles.size() << " cycles\n";
+                //api.get_ob(arb.extract_unique_pairs(cycles), arb);
+
+                for(auto cycle : cycles) {
+                    std::cout << ">>> CYCLE: " << cycle2string(cycle) << " gain=" << gain(arb, cycle) << '\n';
+
+                    //for(auto p_it = cycle.begin(); p_it != std::prev(cycle.end()); ++p_it) {
+                    for(size_t i = 0; i < cycle.size(); ++i) {
+                        auto p = *cycle.begin();
+                        //arb.change_rate(p, gain_r(arb.ob_size(p, 0, true)));
+
+                        for(auto nxt_p = std::next(cycle.begin()); nxt_p != cycle.end(); ++nxt_p) {
+                            for(size_t j = 0; j < arb(p).ob.size(); ++j) {
+                                //auto sizes = arb.sizing_fixed(cycle, *nxt_p, arb.ob_size(*nxt_p, j), {{p, gain_r(arb.ob_size(p, 0, true))}});
+                                cycleSize_t sizes = arb.sizing(cycle, *nxt_p, arb.ob_size(*nxt_p, j)); 
+                                arb.minsize(cycle, sizes);
+                                arb.align_sizes(cycle, sizes);
+                                double profit = calc_profit(cycle, sizes);
+
+                                if(profit > 0 && arb.is_aligned(cycle, sizes)) {
+                                    std::cout << "locked " << p << ", @rate=" << arb(p).rate << ", maximizing " << *nxt_p << ", row=" << j << " ->" << arb.ob_size(*nxt_p, j) << " gain=" << gain(arb, cycle) << " real_gain=" << calc_gain(cycle, sizes) << "\n";
+                                    std::cout << print_sizes(arb, cycle, sizes) << " profit=" << profit << '\n';
+                                    scope.wait();
+                                    if(balance.count(as_pair(cycle[0]).first))
+                                        if(balance[as_pair(cycle[0]).first] >= sizes[cycle[0]].first) {
+                                            cs[cycle2string(cycle)] = sizes;
+                                            std::cout << "groups: " << groupping(cycle, sizes, balance) << '\n';
+                                        }
+                                }
                             }
                         }
 
+                        //arb.change_rate(p, gain_r(arb.ob_size(p, 0, false)));
+                        std::rotate(cycle.begin(), std::next(cycle.begin()), cycle.end());
                     }
-                    
-                    arb.change_rate(p, gain_r(arb.ob_size(p, 0, false)));
-                    std::rotate(cycle.begin(), std::next(cycle.begin()), cycle.end());
+
+                    /*std::cout<<"DOMS:\n";
+                    for(auto p: cycle) {
+                        for(size_t i = 0; i < arb(p).ob.size(); ++i) {
+                            if(i == 0) std::cout<<p<<": "<<arb.ob(p, 0, true)<<" %in sz% "<<arb.ob_size(p, 0, true)<<"\n-----------------\n";
+                            std::cout<<p<<": "<<arb.ob(p, i)<<" %in sz% "<<arb.ob_size(p, i)<<'\n';
+                        }
+                        std::cout<<'\n';
+                    }*/
+                    //break;
+                    std::cout << '\n';
                 }
-                /*std::cout<<"DOMS:\n";
-                for(auto p: cycle) {
-                    for(size_t i = 0; i < arb(p).ob.size(); ++i) {
-                        if(i == 0) std::cout<<p<<": "<<arb.ob(p, 0, true)<<" %in sz% "<<arb.ob_size(p, 0, true)<<"\n-----------------\n";
-                        std::cout<<p<<": "<<arb.ob(p, i)<<" %in sz% "<<arb.ob_size(p, i)<<'\n';
+                std::vector<std::pair<std::string, cycleSize_t>> vcs(cs.begin(), cs.end());
+                std::sort(vcs.begin(), vcs.end(), [&](auto& a, auto& b) {
+                    double p_a = calc_profit(string2cycle(a.first), a.second);
+                    double p_b = calc_profit(string2cycle(b.first), b.second);
+                    if(arb(*string2cycle(a.first).rbegin()).c2 != "usd") p_a *= arb(arb(*string2cycle(a.first).rbegin()).c2+"-usd").rate;
+                    if(arb(*string2cycle(b.first).rbegin()).c2 != "usd") p_b *= arb(arb(*string2cycle(b.first).rbegin()).c2+"-usd").rate;
+
+                    return p_a > p_b;
+                });
+                if(vcs.size()) {
+                    std::cout<<"Profitable cycles and sizes:\n";
+                    for(auto item: vcs) {
+                        std::cout<<item.first<<" sz:"<<print_sizes(arb, string2cycle(item.first), item.second)<<" profit="<<calc_profit(string2cycle(item.first), item.second)<<'\n';
                     }
-                    std::cout<<'\n';
-                }*/
-                //break;
-                std::cout<<'\n';
+                }
+                std::cout << "balance: " << balance << '\n';
+                //std::cout<<"balance:"<<api.get_balance()<<'\n'<<'\n';
+                api.save_nonces();
+                sleep(1s);
             }
-            std::cout<<"balance: "<<balance<<'\n';
-            //std::cout<<"balance:"<<api.get_balance()<<'\n'<<'\n';
-            api.save_nonces();
-            sleep(1s);
-        }
+
+            scope.wait();
+        };
         sched.terminate();
     });
 
